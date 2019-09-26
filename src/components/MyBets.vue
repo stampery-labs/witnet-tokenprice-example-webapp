73ba831c33b3500fd476e6e85fb28bc44582cf05
tomastrg <tomastrg22@gmail.com>
2019-09-26 10:45:47 +0200

    add my bets component

---
+++ src/components/MyBets.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+  <!-- <p class="title">My bets</p> -->
+    <v-data-table
+      :headers="headers"
+      :items="bets"
+      items-per-page="5"
+      height="300px"
+  ></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'youtBets',
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Crypto asset',
+          align: 'left',
+          sortable: true,
+          value: 'ticker'
+        },
+        {
+          text: 'Amount bet',
+          align: 'left',
+          sortable: true,
+          value: 'amount'
+        }
+      ],
+      bets: [
+        {
+          ticker: 'ETH',
+          amount: 1
+        },
+        {
+          ticker: 'REP',
+          amount: 2
+        },
+        {
+          ticker: 'LINK',
+          amount: 1
+        },
+        {
+          ticker: 'BTC',
+          amount: 3
+        },
+        {
+          ticker: 'ETH',
+          amount: 1
+        },
+        {
+          ticker: 'REP',
+          amount: 2
+        },
+        {
+          ticker: 'LINK',
+          amount: 1
+        },
+        {
+          ticker: 'BTC',
+          amount: 3
+        },
+        {
+          ticker: 'ETH',
+          amount: 1
+        },
+        {
+          ticker: 'REP',
+          amount: 2
+        },
+        {
+          ticker: 'LINK',
+          amount: 1
+        },
+        {
+          ticker: 'BTC',
+          amount: 3
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  width: 400px;
+  height: 440px;
+  background-color:azure;
+}
+</style>
